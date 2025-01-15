5107b8df25d2ba210839e09aa6c46c9431f78f6d
tristeceratops <eric.woillard@gmail.com>
2025-01-15 15:46:52 +0100

    update error identify

--- ex02/identify.cpp
+++ ex02/identify.cpp
@@ -6,20 +6,44 @@
 
 void identify(Base *p)
 {
-	if (dynamic_cast<A*>(p))
+	if (dynamic_cast<A*>(p) != NULL)
 		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B*>(p))
+	else if (dynamic_cast<B*>(p) != NULL)
 		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C*>(p))
+	else if (dynamic_cast<C*>(p) != NULL)
 		std::cout << "C" << std::endl;
+	else
+		std::cout << "NULL" << std::endl;
 }
 
 void identify(Base &p)
-{
-	if (dynamic_cast<A*>(&p))
-		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B*>(&p))
-		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C*>(&p))
-		std::cout << "C" << std::endl;
+{	
+	try
+	{
+		if (dynamic_cast<A*>(&p))
+			std::cout << "A" << std::endl;
+	}
+	catch(const std::bad_cast& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		if (dynamic_cast<B*>(&p))
+			std::cout << "B" << std::endl;
+	}
+	catch(const std::bad_cast& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		if (dynamic_cast<C*>(&p))
+			std::cout << "C" << std::endl;
+	}
+	catch(const std::bad_cast& e)
+	{
+		std::cerr << "Bad cast: " << e.what() << '\n';
+	}
+	
 }
