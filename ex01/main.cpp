ab7242d172f928c219d2ab1918a8807fc9d39e8a
tristeceratops <eric.woillard@gmail.com>
2025-02-07 11:03:19 +0100

    main serial

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -15,5 +15,17 @@ int main()
 	std::cout << "str: " << ptr->str << std::endl;
 	std::cout << "money: " << ptr->money << std::endl;
 
+	Data data2;
+	data2.width = 2147483647;
+	data2.str = "";
+	data2.money = -80.8888888888888f;
+
+	uintptr_t raw2 = Serializer::serialize(&data2);
+	Data *ptr2 = Serializer::deserialize(raw2);
+
+	std::cout << "width: " << ptr2->width << std::endl;
+	std::cout << "str: " << ptr2->str << std::endl;
+	std::cout << "money: " << ptr2->money << std::endl;
+
 	return 0;
 }
